d920475315d80f5924fd63333068a0dfb974212c
AjinkyaBankar <77496039+AjinkyaBankar@users.noreply.github.com>
2023-07-09 11:44:18 -0400

    Update fundamental_analysis_app.py

--- fundamental_analysis_app.py
+++ fundamental_analysis_app.py
@@ -46,7 +46,7 @@ if url:
     price_df = s_name.history(period="1000mo")
     if len(price_df) == 0:
         print('Ticker doesnt exists')
-        st.warning('Unable to fetch this ticker's data. Please try other stocks. Thank you for understanding!', icon="🚨")
+        st.warning('Unable to fetch this stock data. Please try other stocks. Thank you for understanding!', icon="🚨")
         sys.exit()
     else:
         st.warning('Working on your request..... Fetching data..... Thanks for your patience!')
